711e2dae0fdbfa0ef01aad35fe1dbdf306eb3206
akshay09surve <akshaysurve.sv@gmail.com>
2024-01-27 00:20:08 +0530

    added

---
+++ Mosh_Course/Exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1
+count = 0
+for num in range(1,11):
+    if(num%2==0):
+        print(num)
+        count+=1
+print("We have",count,"even numbers")
+
+
+# Exercise 2
---
+++ Mosh_Course/greetFunc.py
@@ -0,0 +1,7 @@
+def greet(fname,lname):
+    print(f"Hi {fname} {lname}")
+    print("Welcome aboard")
+
+
+greet("Akshay","Surve")
+greet("Yash","Todankar")
