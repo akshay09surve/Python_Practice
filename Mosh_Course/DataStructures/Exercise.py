02769ce02ac882a1e35efd3a0aff37b49a85cb84
akshay09surve <akshaysurve.sv@gmail.com>
2024-01-29 17:40:06 +0530

    Files added

--- Mosh_Course/DataStructures/Exercise.py
+++ Mosh_Course/DataStructures/Exercise.py
@@ -1,3 +1,19 @@
 # To find the most repeated character in the string below
 
-sentence = "This is a common interview questions"
+sentence = "This is a common interview question"
+# # To convert the string into a list of characters
+# sent = list(sentence)
+
+# # To remove the white spaces from tbe sentence char list
+# for x in sent:
+#     if x==" ":
+#         sent.remove(" ")
+
+
+# print(sent)
+# print(type(str(sent)))
+# newSent = str(sent)
+# print(newSent)
+
+dict1 = {ch:sentence.count(ch) for ch in sentence if ch!=" "}
+print(dict1)
